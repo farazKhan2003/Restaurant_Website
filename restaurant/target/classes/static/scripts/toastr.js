9d44f99db91ad972a4495dee5a356557635b34e3
farazKhan2003 <kfaraz@hotmail.co.uk>
2024-02-01 15:29:23 +0000

    cleaned push

--- restaurant/target/classes/static/scripts/toastr.js
+++
@@ -1,6 +0,0 @@
-// Customisations for toastr
-toastr.options = {
-    "positionClass": "toast-bottom-right",
-    "showMethod": "fadeIn",
-    "hideMethod": "fadeOut"
-}
--- restaurant/target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"project.restaurant"},{"l":"project.restaurant.controllers"},{"l":"project.restaurant.models"},{"l":"project.restaurant.repository"}];updateSearchResults();
